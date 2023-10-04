7c3151ab79ddf56dd85a618cb45a285dcd67fcee
[takafumi_iida] <[s1080012@gmail.com]>
2023-10-04 10:27:02 +0900

    modify ListPage ListTile classification

--- lib/list_page.dart
+++ lib/list_page.dart
@@ -5,19 +5,53 @@ class ListPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    final items = List<String>.generate(100, (i) => "Item $i");
+    final items = List<String>.generate(50, (i) => "Item $i");
+
+    //直書きタップ用メソッド
+    // void onTap(){
+    //   debugPrint("1");
+    // }
 
     return Scaffold(
       body: Container(
         width: double.infinity,
         child: ListView.builder(
            itemCount: items.length,
-           itemBuilder: (context, index) => ListTile(
-             leading: const Icon(Icons.map),
-             title: Text("Map $index")
-           )
+           // 処理を分ける
+           itemBuilder: (_, i) => ListPageListTile(i),
+           // 直書き
+           // itemBuilder: (context, index) => ListTile(
+           //   leading: const Icon(Icons.map),
+           //   title: Text("Map $index"),
+           //   selectedColor: Colors.blue,
+           //   onTap: onTap,
+           // )
          )
       )
+    );
+  }
+}
+
+class ListPageListTile extends StatelessWidget{
+  final int i;
+
+  const ListPageListTile(this.i);
+
+  void onTap(){
+    debugPrint("onTap $i");
+  }
+
+  void onLongPress(){
+    debugPrint("onLongPress $i $key");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: const Icon(Icons.map),
+      title: Text("Map $i"),
+      onTap: onTap,
+      onLongPress: onLongPress,
     );
   }
 }
